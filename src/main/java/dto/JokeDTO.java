e418a8f7d49a4d845c036c83a01a921a58a5e60c
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-10 22:08:40 +0200

    Update JokeDTO.java

--- src/main/java/dto/JokeDTO.java
+++ src/main/java/dto/JokeDTO.java
@@ -5,6 +5,7 @@
  */
 package dto;
 
+import entities.Joke;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,45 +16,21 @@ import javax.persistence.Id;
  *
  * @author benjaminbajrami
  */
-@Entity
-public class JokeDTO implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class JokeDTO {
+    
+    private String joke;
+    private String reference;
+    private double rating;
 
-    public Long getId() {
-        return id;
+    public JokeDTO() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public JokeDTO(Joke joke) {
+        this.joke = joke.getJoke();
+        this.reference = joke.getReference();
+        this.rating = joke.getRating();
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof JokeDTO)) {
-            return false;
-        }
-        JokeDTO other = (JokeDTO) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "dto.JokeDTO[ id=" + id + " ]";
-    }
     
 }
