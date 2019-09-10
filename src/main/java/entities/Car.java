dc1fc6257f66a06263107ea007a8fecfb24ad610
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-10 22:05:14 +0200

    Update Car.java

--- src/main/java/entities/Car.java
+++ src/main/java/entities/Car.java
@@ -24,7 +24,18 @@ public class Car implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private int year;
+    private String make;
+    private String model;
+    private int price;
 
+    public Car(int year, String make, String model, int price) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.price = price;
+    }
+    
     public Long getId() {
         return id;
     }
@@ -33,6 +44,39 @@ public class Car implements Serializable {
         this.id = id;
     }
 
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
     @Override
     public int hashCode() {
         int hash = 0;
