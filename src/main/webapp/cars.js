458f42e6c3cbe4da881519528e4d7d9672bdc988
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-12 23:44:26 +0200

    cars filter test

--- src/main/webapp/cars.js
+++ src/main/webapp/cars.js
@@ -4,20 +4,40 @@
  * and open the template in the editor.
  */
 function getCars() {
-    fetch("https://kodebanditterne.dk/CA-1/api/joke/all")
-  .then(res => res.json())
-  .then(data => {
-   
-    let rows = data.map(function(name) {
-        let row="<tr> <td>"+name.year+"</td> <td>"+name.make+
-                "</td> <td>"+name.model+"</td><td>"+name.price+"</td> </tr>";
-        return row;
-    })
-    
-    let rowsAsString = rows.join("");
-    document.getElementById("tbody").innerHTML = rowsAsString;
-})
+    fetch("https://kodebanditterne.dk/CA-1/api/car/all")
+            .then(res => res.json())
+            .then(data => {
+
+                let rows = data.map(function (name) {
+                    let row = "<tr> <td>" + name.year + "</td> <td>" + name.make +
+                            "</td> <td>" + name.model + "</td><td>" + name.price + "</td> </tr>";
+                    return row;
+                })
+
+                let rowsAsString = rows.join("");
+                document.getElementById("tbody").innerHTML = rowsAsString;
+            })
 }
 
 document.getElementById("button").onclick = getCars;
 
+function getfilterCars() {
+    fetch("https://kodebanditterne.dk/CA-1/api/car/all")
+            .then(res => res.json())
+            .then(data => {
+
+                let filter = data.filter(car => car.price < document.getElementById("carPrice").value)
+                {
+                    let rows = filter.map(function (name) {
+                        let row = "<tr> <td>" + name.year + "</td> <td>" + name.make +
+                            "</td> <td>" + name.model + "</td><td>" + name.price + "</td> </tr>";
+                        return row;
+                    })
+                }
+
+                let rowsAsString = rows.join("");
+                document.getElementById("tbody").innerHTML = rowsAsString;
+            })
+        }
+
+        document.getElementById("filter").onclick = getfilterCars();
