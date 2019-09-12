744779c945d1bbca7aeb183eb05add37b4889cbd
Iben A <cph-ia62@cphbusiness.dk>
2019-09-12 15:52:14 +0200

    Update CarResourceTest.java

--- src/test/java/rest/CarResourceTest.java
+++ src/test/java/rest/CarResourceTest.java
@@ -1,13 +1,18 @@
 package rest;
 
+import entities.Car;
 import entities.Student;
+import facades.Carfacade;
 import utils.EMF_Creator;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.given;
 import io.restassured.parsing.Parser;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import javax.ws.rs.core.UriBuilder;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.util.HttpStatus;
@@ -34,6 +39,9 @@ public class CarResourceTest {
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
     private static EntityManagerFactory emf;
+    private static Car car;
+    private static Car car2;
+    private static List<Car> cars;
 
     static HttpServer startServer() {
         ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
@@ -47,63 +55,71 @@ public class CarResourceTest {
         //NOT Required if you use the version of EMF_Creator.createEntityManagerFactory used above        
         //System.setProperty("IS_TEST", TEST_DB);
         //We are using the database on the virtual Vagrant image, so username password are the same for all dev-databases
-        
         httpServer = startServer();
-        
+
         //Setup RestAssured
         RestAssured.baseURI = SERVER_URL;
         RestAssured.port = SERVER_PORT;
-   
+
         RestAssured.defaultParser = Parser.JSON;
     }
-    
+
     @AfterAll
-    public static void closeTestServer(){
+    public static void closeTestServer() {
         //System.in.read();
-         httpServer.shutdownNow();
+        httpServer.shutdownNow();
     }
-    
-    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
-    //TODO -- Make sure to change the script below to use YOUR OWN entity class
+
     @BeforeEach
     public void setUp() {
+        car = new Car(2008, "Ford", "Mustang GT500", 430000);
+        car2 = new Car(1968, "Ford", "Mustang GT500", 350000);
+        Car car3 = new Car(2010, "Porsche", "Cayenne", 539000);
+        cars = new ArrayList<>();
+        cars.add(car);
+        cars.add(car2);
+        cars.add(car3);
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-            em.createNamedQuery("Car.deleteAllRows").executeUpdate();
-//            em.persist(new Student("Some txt","More text"));
-//            em.persist(new Student("aaa","bbb"));
-           
+            Query query = em.createNativeQuery("truncate table CA1_test.CAR");
+            query.executeUpdate();
+            em.getTransaction().commit();
+
+            em.getTransaction().begin();
+            em.persist(car);
+            em.persist(car2);
+            em.persist(car3);
             em.getTransaction().commit();
         } finally {
             em.close();
         }
     }
-    
+
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
         given().when().get("/car").then().statusCode(200);
     }
-   
+
     //This test assumes the database contains two rows
     @Test
     public void testDummyMsg() throws Exception {
         given()
-        .contentType("application/json")
-        .get("/car/").then()
-        .assertThat()
-        .statusCode(HttpStatus.OK_200.getStatusCode())
-        .body("msg", equalTo("Hello World"));   
+                .contentType("application/json")
+                .get("/car/").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("msg", equalTo("Hello World"));
     }
-    
+
     @Test
     public void testCount() throws Exception {
         given()
-        .contentType("application/json")
-        .get("/car/count").then()
-        .assertThat()
-        .statusCode(HttpStatus.OK_200.getStatusCode())
-        .body("count", equalTo(2));   
+                .contentType("application/json")
+                .get("/car/count").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("count", equalTo(2));
     }
 }
