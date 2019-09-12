4309158ebfdc01f86df571d99b19a688d6a481f4
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-13 00:45:42 +0200

    car update

--- src/main/webapp/cars.js
+++ src/main/webapp/cars.js
@@ -20,24 +20,3 @@ function getCars() {
 }
 
 document.getElementById("button").onclick = getCars;
-
-function getfilterCars() {
-    fetch("https://kodebanditterne.dk/CA-1/api/car/all")
-            .then(res => res.json())
-            .then(data => {
-
-                let filter = data.filter(car => car.price < document.getElementById("carPrice").value)
-                {
-                    let rows = filter.map(function (name) {
-                        let row = "<tr> <td>" + name.year + "</td> <td>" + name.make +
-                            "</td> <td>" + name.model + "</td><td>" + name.price + "</td> </tr>";
-                        return row;
-                    })
-                }
-
-                let rowsAsString = rows.join("");
-                document.getElementById("tbody").innerHTML = rowsAsString;
-            })
-        }
-
-        document.getElementById("filter").onclick = getfilterCars();
