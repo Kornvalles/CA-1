5e8d3f966f183d07a3c66d269ce9eb763ffe0cf4
TheDanishWonder <44898418+TheDanishWonder@users.noreply.github.com>
2019-09-12 21:41:46 +0200

    Revert "Revert "rest point populate all tables""
    This reverts commit 259f264c0df5d259b06d086cba48bbe26d70907b.

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -23,6 +23,7 @@ public class ApplicationConfig extends Application {
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.Carresource.class);
         resources.add(rest.Jokeresource.class);
+        resources.add(rest.Populateresource.class);
         resources.add(rest.Studentresource.class);
     }
     
