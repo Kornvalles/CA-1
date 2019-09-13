ec4a431e193f4ac274797eb820652b673f665bf0
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-13 11:55:41 +0200

    Js filter

--- src/main/webapp/cars.js
+++ src/main/webapp/cars.js
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* global fetch */
+
 function getCars() {
     fetch("https://kodebanditterne.dk/CA-1/api/car/all")
             .then(res => res.json())
@@ -12,31 +14,11 @@ function getCars() {
                     let row = "<tr> <td>" + name.year + "</td> <td>" + name.make +
                             "</td> <td>" + name.model + "</td><td>" + name.price + "</td> </tr>";
                     return row;
-                })
+                });
 
                 let rowsAsString = rows.join("");
                 document.getElementById("tbody").innerHTML = rowsAsString;
-            })
+            });
 }
 
 document.getElementById("button").onclick = getCars;
-
-function filterCars() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("carMakeInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("carTable");
-    tr = table.getElementsByTagName("tr");
-
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
