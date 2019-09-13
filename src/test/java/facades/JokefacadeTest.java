7165ee5f3194de8541e74c76ab8d96f70bf025e2
Mikkel Kornval Christoffersen <mikkelkornval@gmail.com>
2019-09-13 11:26:29 +0200

    Update JokefacadeTest.java

--- src/test/java/facades/JokefacadeTest.java
+++ src/test/java/facades/JokefacadeTest.java
@@ -20,7 +20,7 @@ import utils.EMF_Creator.DbSelector;
 import utils.EMF_Creator.Strategy;
 
 //Uncomment the line below, to temporarily disable this test
-//@Disabled
+@Disabled
 public class JokefacadeTest {
 
     private static EntityManagerFactory emf;
