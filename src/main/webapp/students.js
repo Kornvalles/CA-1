0cc23be07789e1a51b736fe5508615bf9dd604d5
Iben A <cph-ia62@cphbusiness.dk>
2019-09-16 08:22:55 +0200

    Update students.js

--- src/main/webapp/students.js
+++ src/main/webapp/students.js
@@ -1,19 +1,17 @@
 function getStudents() {
     fetch("https://kodebanditterne.dk/CA-1/api/student/all")
-  .then(res => res.json())
-  .then(data => {
-   
-    let rows = data.map(function(name) {
-        let row="<tr> <td>"+name.username+"</td> <td>"+name.name+
-                "</td> <td>"+name.color+"</td> </tr>";
-        return row;
-    })
-    
-    let rowsAsString = rows.join("");
-    let rowAsThead = "<tr><th>Student Id</th><th>Name</th><th>Color</th></tr>";
-        document.getElementById("thead").innerHTML = rowAsThead;
-        document.getElementById("tbody").innerHTML = rowsAsString;
-})
+            .then(res => res.json())
+            .then(data => {
+
+                let rows = data.map(function (name) {
+                    let row = "<tr> <td>" + name.username + "</td> <td>" + name.name +
+                            "</td> <td>" + name.color + "</td> </tr>";
+                    return row;
+                })
+
+                let rowsAsString = rows.join("");
+                document.getElementById("tbody").innerHTML = rowsAsString;
+            })
 }
 
 document.getElementById("button").onclick = getStudents;
